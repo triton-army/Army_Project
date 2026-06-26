4d2951e8d1ebc715e05457b3909f5b6efc42d565
triton <triton45677@gmail.com>
2026-06-26 23:58:56 +0900

    main screen

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,9 @@
 const token = localStorage.getItem("token");
 
 if (!token) {
+    alert("로그인이 필요합니다.");
     location.href = "login.html";
 }
-
-fetch("/auth/me", {
-    headers: {
-        Authorization: `Bearer ${token}`
-    }
-})
-.then(res => {
-    if (!res.ok) {
-        localStorage.removeItem("token");
-        location.href = "login.html";
-    }
-
-    return res.json();
-})
-.then(data => {
-    console.log(data.user);
-});
 
 document.getElementById("logoutBtn").addEventListener("click", () => {
     localStorage.removeItem("token");
